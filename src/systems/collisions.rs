dde316c654d560cdbdc1ffd282c1631ba5a4fb39
Doct0rLekter <marikwinters@gmail.com>
2023-06-15 15:55:21 -0400

    Implement collision system

--- src/systems/collisions.rs
+++ src/systems/collisions.rs
@@ -1,9 +1,21 @@
-use std::process::Command;
-
 use crate::prelude::*;
 
-#[sytem]
-#[read_component(Point, Player, Enemy)]
+#[system]
+#[read_component(Point)]
+#[read_component(Player)]
+#[read_component(Enemy)]
 pub fn collisions(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
-    
+    let mut player_position = Point::zero();
+    let mut players = <&Point>::query().filter(component::<Player>());
+
+    players.iter(ecs).for_each(|pos| player_position = *pos);
+
+    let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+
+    enemies
+        .iter(ecs)
+        .filter(|(_, pos)| **pos == player_position)
+        .for_each(|(entity, _)| {
+            commands.remove(*entity);
+        });
 }
